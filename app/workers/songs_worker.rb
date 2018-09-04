b54aad5ade3654ed82b6f71413ed8702f477b6e8
Victoria Meng <nights1ststar@protonmail.com>
2018-09-04 18:57:28 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -3,5 +3,8 @@ class SongsWorker
   include Sidekiq::Worker
 
   def perform(file)
+    CSV.foreach(file, headers: true) do |song| 
+      Song.create(title: song[0], artist_name: song[1])
+    end
   end
 end
